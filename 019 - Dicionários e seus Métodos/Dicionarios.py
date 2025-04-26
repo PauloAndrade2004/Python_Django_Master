637dab4120a7b5a642a45491a8bcce0db111d68a
PauloCesar <pauloandradeoliveira2004@gmail.com>
2025-04-25 23:15:18 -0300

    Crando meu primeiro Dicionário em Python, Sintaxe

---
+++ 019 - Dicionários e seus Métodos/Dicionarios.py
@@ -0,0 +1,14 @@
+# Nos dicionario os elementos são chave e valor
+# Chave é o que identifica o elemento 
+# E o valor é o que ele representa
+
+# Sintaxe do dicionário.
+meu_dicio = {}
+
+#                  Chaves  Valore  Chave  Valor |  Chave   valor
+meu_dicionario = {'Nome': 'Paulo', 'Idade': 20, 'profissão': 'Dev', 'Cidade': 'São Paulo'}
+
+print(meu_dicionario['Nome'])  # Acessando o valor da chave 'Nome'
+
+
+
