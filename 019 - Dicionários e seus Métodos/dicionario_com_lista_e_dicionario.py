bdf81b31ada9e5adab7c873355ad3474689e70e8
PauloCesar <pauloandradeoliveira2004@gmail.com>
2025-04-25 23:17:39 -0300

    Estudamos Dicionário com listas e outros dicionários

---
+++ 019 - Dicionários e seus Métodos/dicionario_com_lista_e_dicionario.py
@@ -0,0 +1,20 @@
+
+meu_dicionario = {
+    # Chaves         Valores
+    'Nome':         'Paulo',
+    'Idade':        20,
+    'Profissão':    'Dev',
+
+    # Valor é uma lista
+    'Interesses': ['Python', 'Java'],
+    
+    # Este dicionário armazena pares de chave e valor.
+    # As chaves são strings, e os valores podem ser de qualquer tipo — até mesmo listas ou outros dicionários.
+
+    # Um dicionário pode conter outro dicionário como valor de uma chave.
+    'Endereco': {
+        'Rua': 'Rua Sanhaçu',
+        'Número': 310,
+        'Cidade': 'São Paulo'
+    }
+}
