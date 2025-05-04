95e630576bb924a1c2222bbfccd16e26914ebc76
PauloCesar <pauloandradeoliveira2004@gmail.com>
2025-05-04 17:44:33 -0300

    Adicionamos um elemento dentro do nosso dicionario.

---
+++ 019 - Dicionários e seus Métodos/adicionando_elemento_dicionario.py
@@ -0,0 +1,17 @@
+meu_dicionario = {
+    # Chaves         Valores
+    'Nome':         'Paulo',
+    'Idade':        20,
+    'Profissão':    'Dev',
+    'pet': {
+        'cachorro': 'Loki',
+        'raca': 'Cani Corso',
+        'idade cachorro': '8'
+    },
+}
+
+# Adicionando uma nova (chave) é um novo (valor) dentro do nosso dicionario.
+    # quando eu faço isso eu estou adicionando uma nova chave e um novo valor, ao meu dicionario. Abrimos colchetes e dentro adicionamos,
+    # o a nossa (chave).
+meu_dicionario['ano_nascimento'] = 2004
+print(meu_dicionario)
